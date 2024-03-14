1b595638d1beab7c4fda6847dc47083e4535c053
Mahalakshmi03935 <rajamahalakshmi.b@diggibyte.com>
2024-03-14 23:54:15 +0530

    Implemented print_formatted function

--- src/python_string_formatting/util.py
+++ src/python_string_formatting/util.py
@@ -1,1 +1,13 @@
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+
+def print_formatted(number):
+
+    width = len(bin(number)[2:])
+    for i in range(1, number + 1):
+        decimal = str(i).rjust(width)
+        octal = oct(i)[2:].rjust(width)
+        hexadecimal = hex(i)[2:].upper().rjust(width)
+        binary = bin(i)[2:].rjust(width)
+        logging.info(f"{decimal} {octal} {hexadecimal} {binary}")
 
