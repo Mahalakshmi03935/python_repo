c3b0d798788735f078588f7e27ae3f16122de534
Mahalakshmi03935 <rajamahalakshmi.b@diggibyte.com>
2024-03-14 20:41:01 +0530

    Implemented the calculate_time_difference function

--- src/time_delta/driver.py
+++ src/time_delta/driver.py
@@ -1,1 +1,27 @@
+import util
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def calculate_time_difference(testcases):
+    results = []
+    for time1, time2 in testcases:
+        difference = util.get_time_difference_in_seconds(time1, time2)
+        if difference is not None:
+            results.append(difference)
+    return results
+
+if __name__ == "__main__":
+    testcases = [
+        ("Sun 10 MAY 2015 13:54:36 -0700", "Sun 10 May 2015 13:54:36 -0000"),
+        ("Sat 02 May 2015 19:54:36 +0530", "Fri 01 May 2015 13:54:36 -0000"),
+        # Add more test cases if needed
+    ]
+
+
+    results = calculate_time_difference(testcases)
+    if results:
+        for result in results:
+            logger.info("Time difference in seconds: {}".format(result))
 
--- src/time_delta/util.py
+++ src/time_delta/util.py
@@ -1,1 +1,13 @@
+from datetime import datetime, timedelta
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def get_time_difference_in_seconds(time1, time2):
+    dt_format = '%a %d %b %Y %H:%M:%S %z'
+    timestamp1 = datetime.strptime(time1, dt_format)
+    timestamp2 = datetime.strptime(time2, dt_format)
+    difference = abs(timestamp1 - timestamp2).total_seconds()
+    return int(difference)
 
