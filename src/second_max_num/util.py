ce20dd8b877f4d9dc3ca32aee6e80625899b0d59
Mahalakshmi03935 <rajamahalakshmi.b@diggibyte.com>
2024-03-14 20:53:20 +0530

    implemented find_runner_up_score function

--- src/second_max_num/util.py
+++ src/second_max_num/util.py
@@ -1,1 +1,12 @@
+import logging
 
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+
+
+# Define utility function to find the runner-up score
+def find_runner_up_score(n,scores):
+    if len(scores) < 2:
+        return None
+    sorted_scores = sorted(scores, reverse=True)
+    runner_up_score = sorted_scores[1]
+    return runner_up_score
