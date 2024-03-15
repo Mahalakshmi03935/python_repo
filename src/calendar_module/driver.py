c56046a274128106d4d7e63302bc7d6bb6d98bf3
Mahalakshmi03935 <rajamahalakshmi.b@diggibyte.com>
2024-03-15 05:44:59 +0530

    Implemented find_day function

--- src/calendar_module/driver.py
+++ src/calendar_module/driver.py
@@ -1,1 +1,13 @@
+import logging
+from util import find_day
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
+input_date = input("Enter the date in format (MM DD YYYY): ")
+parts = input_date.split()
+if len(parts) == 3:
+    month, day, year = map(int, parts)
+    day_of_week = find_day(month, day, year)
+    logger.info(f"The day is: {day_of_week}")
+else:
+    logger.error("Invalid input. Please enter date in format (MM DD YYYY)")
--- src/calendar_module/util.py
+++ src/calendar_module/util.py
@@ -1,1 +1,11 @@
+import logging
+import calendar
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+def find_day(month, day, year):
+    day_of_week = calendar.day_name[calendar.weekday(year, month, day)].upper()
+    logger.info(f"The day on {month}/{day}/{year} was {day_of_week}")
+    return day_of_week
 
