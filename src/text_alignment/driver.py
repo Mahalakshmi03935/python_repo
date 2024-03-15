1cf6d8ba524f267b34708febc4199e72731908f6
Mahalakshmi03935 <rajamahalakshmi.b@diggibyte.com>
2024-03-15 08:09:45 +0530

    Implemented hackerrank_logo

--- src/text_alignment/driver.py
+++ src/text_alignment/driver.py
@@ -1,1 +1,11 @@
+import logging
+from util import generate_hackerrank_logo
+
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    thickness = int(input("Enter the thickness for the logo: "))
+    logo = generate_hackerrank_logo(thickness)
+    logger.debug("\n%s", logo)
 
--- src/text_alignment/util.py
+++ src/text_alignment/util.py
@@ -1,1 +1,39 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+logger = logging.getLogger(__name__)
+
+def generate_hackerrank_logo(thickness):
+    if thickness % 2 == 0:
+        raise ValueError("Thickness must be an odd number")
+
+    logo = ''
+    width = thickness * 3
+
+    # Top Cone
+    for i in range(thickness):
+        line = 'H' * (2 * i + 1)
+        logo += line.center(width, '-') + '\n'
+
+    # Top Pillars
+    for i in range(thickness + 1):
+        line = ('H' * thickness).center(thickness * 2, ' ')
+        logo += line + line + '\n'
+
+    # Middle Belt
+    for i in range((thickness + 1) // 2):
+        logo += ('H' * thickness * 5).center(thickness * 6, ' ') + '\n'
+
+    # Bottom Pillars
+    for i in range(thickness + 1):
+        line = ('H' * thickness).center(thickness * 2, ' ')
+        logo += line + line + '\n'
+
+    # Bottom Cone
+    for i in range(thickness):
+        line = 'H' * (2 * (thickness - i) - 1)
+        logo += line.center(width, '-') + '\n'
+
+    logger.info("Generated HackerRank logo with thickness: %d", thickness)
+    return logo
 
