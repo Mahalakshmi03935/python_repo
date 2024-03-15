80798236b00f151031b53a5f6ba6764a11fa046b
Mahalakshmi03935 <rajamahalakshmi.b@diggibyte.com>
2024-03-15 05:53:17 +0530

    Implemented the mutate string function

--- src/python_mutations/driver.py
+++ src/python_mutations/driver.py
@@ -1,1 +1,8 @@
+import logging
+from util import mutate_string
 
+
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+
+mutated_string = mutate_string('mahalakshmi', 10, 'e')
+logging.debug(mutated_string)
--- src/python_mutations/util.py
+++ src/python_mutations/util.py
@@ -1,1 +1,14 @@
+import logging
 
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+
+def mutate_string(string, position, character):
+    if position < 0 or position >= len(string):
+        logging.error("Invalid position provided.")
+        return string
+    else:
+        string_list = list(string)#mahalakshmi
+        string_list[position] = character#4-->l
+        result = ''.join(string_list)
+        logging.debug(result)
+        return result
