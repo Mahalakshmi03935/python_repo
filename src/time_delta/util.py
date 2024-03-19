d39c0a3799a15122b7a29a15b4746da563aed500
Mahalakshmi03935 <rajamahalakshmi.b@diggibyte.com>
2024-03-19 12:53:32 +0530

    Imported datetime and Calculated time_diff_seconds

--- src/time_delta/util.py
+++ src/time_delta/util.py
@@ -1,13 +1,22 @@
-from datetime import datetime, timedelta
 import logging
+logging.basicConfig(level=logging.DEBUG, format="%(message)s" )
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from datetime import datetime
 
-def get_time_difference_in_seconds(time1, time2):
-    dt_format = '%a %d %b %Y %H:%M:%S %z'
-    timestamp1 = datetime.strptime(time1, dt_format)
-    timestamp2 = datetime.strptime(time2, dt_format)
-    difference = abs(timestamp1 - timestamp2).total_seconds()
-    return int(difference)
+
+# Complete the time_delta function below.
+def time_delta():
+    t = int(input("Enter the number of test cases: "))
+
+    for t_itr in range(t):
+        t1 = input("Enter datetime string 1: ")
+        t2 = input("Enter datetime string 2: ")
+
+    format_str = "%a %d %b %Y %H:%M:%S %z"
+    dt1 = datetime.strptime(t1, format_str)
+    dt2 = datetime.strptime(t2, format_str)
+
+    time_diff_seconds = abs((dt1 - dt2).total_seconds())
+    logging.debug(int(time_diff_seconds))
+    return int(time_diff_seconds)
 
