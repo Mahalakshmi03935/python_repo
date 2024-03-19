bcca02c88e03ecf9c7373834a9884fcf5d19ec60
Mahalakshmi03935 <rajamahalakshmi.b@diggibyte.com>
2024-03-19 12:09:15 +0530

    used regular expressions for validating email address

--- src/email_address/util.py
+++ src/email_address/util.py
@@ -1,9 +1,8 @@
+#imported regular expression module
 import re
-
 def is_valid_email(email):
     pattern = r'^[a-zA-Z0-9_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}$'
     return re.match(pattern, email)
-
 def filter_valid_emails(emails):
     valid_emails = filter(lambda x: is_valid_email(x), emails)
     return sorted(valid_emails)
