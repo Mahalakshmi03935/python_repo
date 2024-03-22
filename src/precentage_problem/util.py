a31749e2050fe8b015e212aecfd96f1636399845
Mahalakshmi03935 <162538876+Mahalakshmi03935@users.noreply.github.com>
2024-03-22 18:34:10 +0530

    Update util.py

--- src/precentage_problem/util.py
+++ src/precentage_problem/util.py
@@ -1,7 +1,7 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format="%(message)s" )
 
-def read_student_marks():
+def student_marks():
     n = int(input("Enter the number of students? "))
     student_marks = {}
     for _ in range(n):
