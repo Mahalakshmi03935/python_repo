312c7966e6b60a345d2000d1c8374596f72a503e
Mahalakshmi03935 <rajamahalakshmi.b@diggibyte.com>
2024-03-23 14:19:17 +0530

    Calculated the Average of the Students

--- src/precentage_problem/driver.py
+++ src/precentage_problem/driver.py
@@ -1,16 +1,2 @@
-from util import calculate_average_marks
-
-def main():
-    n = int(input("Enter the number of students: ").strip())
-    student_records = {}
-    for _ in range(n):
-        name, *marks = input("Enter name and marks separated by space: ").split()
-        student_records[name] = list(map(float, marks))
-    query_name = input("Enter the name of the student to query: ").strip()
-
-    result = calculate_average_marks(student_records, query_name)
-    print(f"The average marks for {query_name} are {result}")
-
-if __name__ == "__main__":
-    main()
-
+from util import student_marks, calculate_average
+student_marks()
--- src/precentage_problem/util.py
+++ src/precentage_problem/util.py
@@ -1,1 +1,16 @@
+import logging
+logging.basicConfig(level=logging.DEBUG, format="%(message)s" )
 
+def student_marks():
+    n = int(input("Enter the number of students? "))
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input("Enter the name of the student and their marks: ").split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    return student_marks
+
+def calculate_average(student_marks, query_name):
+    average_score = sum(student_marks[query_name]) / len(student_marks[query_name])
+    return average_score
+    logging.info(f"The average of {query_name} is {average_score:.2f}")
