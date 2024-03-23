5f1805817ddb48abfd4801031b1601790e06904c
Mahalakshmi03935 <rajamahalakshmi.b@diggibyte.com>
2024-03-23 19:28:35 +0530

    The Function named find_day is used

--- src/calendar_module/driver.py
+++ src/calendar_module/driver.py
@@ -1,13 +1,2 @@
-import logging
-from util import find_day
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
-input_date = input("Enter the date in format (MM DD YYYY): ")
-parts = input_date.split()
-if len(parts) == 3:
-    month, day, year = map(int, parts)
-    day_of_week = find_day(month, day, year)
-    logger.info(f"The day is: {day_of_week}")
-else:
-    logger.error("Invalid input. Please enter date in format (MM DD YYYY)")
+from python_repo.src.calendar_module.util import find_day
+find_day()
--- src/calendar_module/util.py
+++ src/calendar_module/util.py
@@ -1,11 +1,10 @@
 import logging
+logging.basicConfig(level=logging.INFO,format="%(message)s")
 import calendar
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
-def find_day(month, day, year):
-    day_of_week = calendar.day_name[calendar.weekday(year, month, day)].upper()
-    logger.info(f"The day on {month}/{day}/{year} was {day_of_week}")
-    return day_of_week
-
+def find_day():
+    mm, dd, yyyy = map(int, input().split())
+    ind = calendar.weekday(yyyy, mm, dd)
+    days = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"]
+    result = days[ind]
+    logging.info(result)
+    return result
