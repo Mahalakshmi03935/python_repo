548b8b1ab3e14e8c071cff665a32a29dc3db47de
Mahalakshmi03935 <rajamahalakshmi.b@diggibyte.com>
2024-03-25 00:06:06 +0530

    The function named iterators is used

--- src/iterators_problem/util.py
+++ src/iterators_problem/util.py
@@ -1,8 +1,21 @@
 import logging
-#defining calculate_probability function
-def calculate_probability(n, letters, k):
-    total_indices = n
-    indices_with_target_letter = letters.count('a')
-    probability = 1 - ((1 - indices_with_target_letter / total_indices) ** k)
-    return round(probability, 4)
+logging.basicConfig(level=logging.DEBUG, format="%(message)s" )
+from itertools import combinations
+
+def iterators():
+
+    n = list(range(1, int(input()) + 1))
+    b = list(input().split(" "))
+    indx = []
+    for i in range(len(b)):
+        if b[i] == "a":
+            indx.append(i + 1)
+    k = list(combinations(n, int(input())))
+    out = 0
+    for i in k:
+        if any([j in i for j in indx]):
+            out += 1
+    logging.debug(out / len(list(k)))
+    return out / len(list(k))
+iterators()
 
